a93d94206f1a24d3bba322287b58bbaf68209496
Dieg-20 <76624357+Dieg-20@users.noreply.github.com>
2022-06-02 16:12:58 -0400

    Added getSupply function

--- contracts/ERC-721_simpleContract.sol
+++ contracts/ERC-721_simpleContract.sol
@@ -1,24 +1,26 @@
+
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SimpleERC721 is ERC721URIStorage, Ownable  {
+contract SimpleERC721 is ERC721Enumerable, Ownable  {
      using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+     Counters.Counter private _tokenIds;
 
     constructor () public ERC721 ("Collectible", "COLLECTIBLE"){}
 
-    function mint(uint256 quantity) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-
-        for (uint i = 0; i < quantity; i++) {
-          _mint(msg.sender, newItemId);
-          _setTokenURI(newItemId, "");
-          return newItemId;
+    function mint(uint256 quantity) public {
+        for (uint i = 1; i <= quantity; i++) {
+          _tokenIds.increment();
+          uint256 newItemId = _tokenIds.current();
+          _safeMint(msg.sender, newItemId);
         }   
+    }
+
+    function getSupply() public view returns(uint256){
+        return _tokenIds.current();
     }
     
     function withdraw() public onlyOwner  {
@@ -27,3 +29,5 @@ contract SimpleERC721 is ERC721URIStorage, Ownable  {
     }
     
 }
+
+
